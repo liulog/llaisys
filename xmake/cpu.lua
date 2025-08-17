c40b47c19ad7979df126abd19d2b9e2cff0cd753
刘景宇 <2537738252@qq.com>
2025-08-17 11:22:23 +0800

    change to O3 compile for vector (SIMD)

--- xmake/cpu.lua
+++ xmake/cpu.lua
@@ -16,8 +16,18 @@ target("llaisys-ops-cpu")
     add_deps("llaisys-tensor")
     set_languages("cxx17")
     set_warnings("all", "error")
+
+    -- 非 Windows：仅启用 -O3（GCC/Clang 下会自动开启向量化）
     if not is_plat("windows") then
-        add_cxflags("-fPIC", "-Wno-unknown-pragmas")
+        add_cxflags("-fPIC", "-Wno-unknown-pragmas", "-O3", "-fopt-info-vec-optimized")
+    end
+ 
+    -- Windows：
+    -- MSVC 用 /O2；MinGW/Clang on Windows 仍用 -O3
+    if is_plat("windows") then
+        add_cxflags("/O2", {tools = "cl"})       -- MSVC
+        add_cxflags("-O3", {tools = "gcc"})      -- MinGW
+        add_cxflags("-O3", {tools = "clang"})    -- LLVM clang-cl/clang++
     end
 
     add_files("../src/ops/*/cpu/*.cpp")
